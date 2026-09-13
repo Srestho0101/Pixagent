01ddfe9f1b8a81580f5754756d6454bac8e2532f
Srestho0101 <srestho1298@gmail.com>
2026-09-13 12:49:27 +0600

    Fixing CORS issue

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ class Settings:
         origin.strip()
         for origin in os.getenv(
             "FRONTEND_ORIGINS",
-            ""
+            "http://localhost:5500,http://127.0.0.1:5500,https://srestho0101.github.io"
         ).split(",")
         if origin.strip()
     ]
